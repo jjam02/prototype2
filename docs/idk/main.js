a63e397cc09343e5831bcc33704356a14ca2134f
Jonathan Alvarez <jalva100@ucsc.edu>
2023-11-11 21:24:11 -0800

    function that draws mazes is done

--- docs/idk/main.js
+++ docs/idk/main.js
@@ -14,6 +14,29 @@ characters = [
 `,
 ];
 
+let maze = {
+  grid: [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  ],
+
+  scale: 5,
+};
+
 const G = {
   WIDTH: 100,
   HEIGHT: 150,
@@ -49,11 +72,12 @@ function update() {
       isFiringLeft: true,
     };
   }
-  updateRect();
-  if (input.isPressed) {
-    rect(player.pos.x, 20, G.WIDTH - player.pos.x, G.HEIGHT - 20);
-    rect(0, 20, player.pos.x, G.HEIGHT - 20);
-  }
+  drawMaze(maze);
+  //updateRect();
+  // if (input.isPressed) {
+  //   rect(player.pos.x, 20, G.WIDTH - player.pos.x, G.HEIGHT - 20);
+  //   rect(0, 20, player.pos.x, G.HEIGHT - 20);
+  // }
 
   player.pos = vec(input.pos.x, input.pos.y);
 }
@@ -66,6 +90,20 @@ function updateRect() {
   rect(player.pos.x - 10, 20, 20, player.pos.y - 23);
 
   rect(player.pos.x - 10, player.pos.y + 5, 20, G.HEIGHT);
+}
+
+function drawMaze(maze) {
+  maze.grid.forEach(function (row, rowIndex) {
+    row.forEach(function (cell, colIndex) {
+      //console.log(rowIndex, colIndex, "VALUE:", cell);
+
+      if (cell == 0) {
+        color("red");
+        rect(colIndex * maze.scale, rowIndex * maze.scale + 20, maze.scale);
+      }
+    });
+  });
+  //end();
 }
 
 // helper function - generates random number between min and max (inclusive)
