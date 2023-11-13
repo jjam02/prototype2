788a2144bde193d84c80baa53575d81439989b17
Jonathan Alvarez <jalva100@ucsc.edu>
2023-11-12 22:11:19 -0800

    main game loop works

--- docs/idk/main.js
+++ docs/idk/main.js
@@ -1,34 +1,36 @@
-title = "I dont know";
+title = "Maze Quest";
 
-description = ` move mouse
-to bottom
-then click
+description = `  
+  move mouse
+  to bottom
+  then click the x
+
+
+
+            x
 `;
 
 characters = [
   `
-
-
-	ll
-	ll
-	ll
+	yy
+	yy
 `,
 ];
 
 let maze = {
   grid: [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 2, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 2, 0],
+    [0, 1, 1, 1, 0, 0, 1, 0, 0, 2, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0],
+    [0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+    [0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0],
+    [0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 2, 0, 0, 0, 1, 0],
+    [0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 2, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0],
     [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
   ],
 
@@ -38,16 +40,17 @@ let maze = {
 const G = {
   WIDTH: 100,
   HEIGHT: 150,
+  VIEW: 4,
+  VIEW2: 23,
 };
 
 options = {
-  viewSize: { x: G.WIDTH, y: G.HEIGHT },
   isCapturing: true,
   isCapturingGameCanvasOnly: true,
   captureCanvasScale: 2,
   seed: 1,
   isPlayingBgm: true,
-  isReplayEnabled: true,
+  isReplayEnabled: false,
   theme: "dark",
 };
 
@@ -57,23 +60,49 @@ options = {
  * }} Player
  */
 
+/**
+ * @type { rect[] }
+ */
+let collect = [];
+
 let player;
 let width;
-
-options = {};
+let found = 0;
 
 function update() {
   if (!ticks) {
     player = {
       pos: vec(G.WIDTH * 0.5, G.HEIGHT * 0.5),
-      firingCooldown: G.PLAYER_FIRE_RATE,
-      isFiringLeft: true,
     };
-  }
-  console.log(player.pos);
-  drawMaze(maze);
 
-  if (input.isPressed || input.pos.y > 80) {
+    G.WIDTH = 100;
+    G.HEIGHT = 150;
+    G.VIEW = 4;
+    G.VIEW2 = 23;
+    maze.grid = [
+      [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 1, 0, 1, 0, 2, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 2, 0],
+      [0, 1, 1, 1, 0, 0, 1, 0, 0, 2, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0],
+      [0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0],
+      [0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+      [0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+      [0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0],
+      [0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 2, 0, 0, 0, 1, 0],
+      [0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0],
+      [0, 2, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0],
+      [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+      [0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    ];
+  }
+
+  drawMaze(maze);
+  player.pos = vec(input.pos.x, input.pos.y);
+
+  char("a", player.pos);
+  const wall = char("a", player.pos).isColliding.rect.red;
+
+  if (input.isPressed || input.pos.y > 80 || input.pos.y < 20 || wall) {
     // color("black");
     // rect(player.pos.x, 20, G.WIDTH - player.pos.x, G.HEIGHT - 20);
     // rect(0, 20, player.pos.x, G.HEIGHT - 20);
@@ -82,19 +111,39 @@ function update() {
     updateRect();
   }
 
-  player.pos = vec(input.pos.x, input.pos.y);
-  color("yellow");
-  char("a", player.pos);
+  text("Cubes\ncollected:  /6", 3, 10, {
+    color: "black",
+    scale: { x: 1, y: 1 },
+  });
+  console.log(player.pos);
+
+  const grow = char("a", player.pos).isColliding.rect.green;
+
+  if (grow) {
+    play("powerUp");
+    G.VIEW += 1;
+    G.VIEW2 += 0.5;
+    maze.grid[Math.floor((player.pos.y - 21) / maze.scale)][
+      (player.pos.x - 1) / maze.scale
+    ] = 1;
+  }
+
+  if (wall) {
+    play("explosion");
+    color("green");
+    end("Mouse to bottom");
+  }
 }
 
 function updateRect() {
   color("black");
-  rect(player.pos.x + 3, 20, G.WIDTH - player.pos.x, G.HEIGHT - 20);
-  rect(0, 20, player.pos.x - 3, G.HEIGHT - 20);
+  rect(player.pos.x + G.VIEW, 20, G.WIDTH - player.pos.x, G.HEIGHT - 20); //right wall
 
-  rect(player.pos.x - 10, 20, 20, player.pos.y - 23);
+  rect(0, 20, player.pos.x - G.VIEW, G.HEIGHT - 20); // left wall
 
-  rect(player.pos.x - 10, player.pos.y + 5, 20, G.HEIGHT);
+  rect(player.pos.x - 10, 20, 20, player.pos.y - G.VIEW2); // top
+
+  rect(player.pos.x - 10, player.pos.y + 5, 20, G.HEIGHT); // bottom
 }
 
 function drawMaze(maze) {
@@ -105,6 +154,10 @@ function drawMaze(maze) {
       if (cell == 0) {
         color("red");
         rect(colIndex * maze.scale, rowIndex * maze.scale + 20, maze.scale);
+      } else if (cell == 2) {
+        color("green");
+        //console.log("rect,", colIndex, rowIndex);
+        rect(colIndex * maze.scale + 1, rowIndex * maze.scale + 21, 1);
       }
     });
   });
